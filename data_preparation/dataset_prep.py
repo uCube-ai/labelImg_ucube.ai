773f29a3201a6d60de0f86083f4d83b47d282a83
laxman-ai <166994927+dongrelaxman@users.noreply.github.com>
2025-08-06 10:45:20 +0530

    feat: update dataset preparation script with new source and destination paths, adjust dataset split counts, and improve image-label pair collection

--- data_preparation/dataset_prep.py
+++ data_preparation/dataset_prep.py
@@ -4,29 +4,28 @@ import shutil
 import random
 from pathlib import Path
 
-SOURCE_PARENT_DIR = "/Users/laxmandongre/Downloads/individual_folders"
-DEST_DIR = "/Users/laxmandongre/Downloads/dataset_created"
-
+DEST_DIR= "/Users/laxmandongre/Downloads/DATA_set_10th _july_for_train"
+SOURCE_DIR = "/Users/laxmandongre/Downloads/Dataset_10th_july"
 
 # 🔢 Dataset split counts
-NUM_TRAIN = 134
-NUM_VAL = 7
-NUM_TEST = 7
+NUM_TRAIN = 190
+NUM_VAL = 5
+NUM_TEST = 8
 
 def collect_image_label_pairs(source_dir):
     image_extensions = ['.jpg', '.jpeg', '.png']
     image_label_pairs = []
 
-    for subfolder in os.listdir(source_dir):
-        folder_path = os.path.join(source_dir, subfolder)
-        if os.path.isdir(folder_path):
-            for file in os.listdir(folder_path):
-                file_path = os.path.join(folder_path, file)
-                ext = Path(file).suffix.lower()
-                if ext in image_extensions:
-                    label_path = os.path.splitext(file_path)[0] + ".txt"
-                    if os.path.exists(label_path):
-                        image_label_pairs.append((file_path, label_path))
+    for file in os.listdir(source_dir):
+        ext = Path(file).suffix.lower()
+        if ext in image_extensions:
+            image_path = os.path.join(source_dir, file)
+            label_path = os.path.splitext(image_path)[0] + ".txt"
+            if os.path.exists(label_path):
+                image_label_pairs.append((image_path, label_path))
+            else:
+                print(f"⚠️ No label for: {file}")
+    print(f"✅ Found {len(image_label_pairs)} image-label pairs.")
     return image_label_pairs
 
 def split_dataset(pairs, num_train, num_val, num_test):
@@ -50,17 +49,16 @@ def copy_dataset(pairs, image_out_dir, label_out_dir):
         shutil.copy(label, os.path.join(label_out_dir, os.path.basename(label)))
 
 def organize_dataset():
-    all_pairs = collect_image_label_pairs(SOURCE_PARENT_DIR)
+    all_pairs = collect_image_label_pairs(SOURCE_DIR)
     train, val, test = split_dataset(all_pairs, NUM_TRAIN, NUM_VAL, NUM_TEST)
 
-    print(f"Total pairs found: {len(all_pairs)}")
     print(f"→ Train: {len(train)}\n→ Val: {len(val)}\n→ Test: {len(test)}")
 
     copy_dataset(train, os.path.join(DEST_DIR, "images/train"), os.path.join(DEST_DIR, "labels/train"))
     copy_dataset(val, os.path.join(DEST_DIR, "images/val"), os.path.join(DEST_DIR, "labels/val"))
     copy_dataset(test, os.path.join(DEST_DIR, "images/test"), os.path.join(DEST_DIR, "labels/test"))
 
-    print("✅ Done organizing the dataset!")
+    print("✅ Dataset organized!")
 
 if __name__ == "__main__":
     organize_dataset()
